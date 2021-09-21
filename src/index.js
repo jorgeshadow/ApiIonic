139af43a355d355d46a0bad4d469ef45670ecd6b
jorge luis sanchez ramirez <42231910+jorgeshadow@users.noreply.github.com>
2021-09-21 13:55:13 -0500

    make it bettersds

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const jwt = require('jsonwebtoken');
 const express =require('express')
 const app = express();
 const cors = require('cors')
-app.use(cors())
+app.use(cors({
+    "origin":"*",
+    'Access-Control-Allow-Origin': '*'
+}))
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log('server on port '+port)
